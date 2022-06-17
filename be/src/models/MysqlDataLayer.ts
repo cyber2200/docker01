5143a889222dfbdcf521d991bb00f250c3d878da
Ran Argaman <ranware2200@yahoo.com>
2022-06-17 16:02:22 +0300

    .

--- be/src/models/MysqlDataLayer.ts
+++ be/src/models/MysqlDataLayer.ts
@@ -1,7 +1,7 @@
 export class MysqlDataLayer {
     async getMysqlTestRes() {
       const mysql = require('mysql2/promise');
-      const connection = await mysql.createConnection({ host: 'dct_db01', user: 'dct_db01', password: '123qwe', database: 'dct_db01' });
+      const connection = await mysql.createConnection({ host: 'dct_mysql01', user: 'dct_mysql01', password: '123qwe', database: 'dct_mysql01' });
       const res = await connection.query('SELECT * FROM `event_log`;');
       await connection.end();
       const ret = { rows: [] };
--- be/src/models/PgDataLayer.ts
+++ be/src/models/PgDataLayer.ts
@@ -4,14 +4,14 @@ export class PgDataLayer {
       const delay: any = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
       await delay(1000)
       const client: any = new Client({
-        user: 'pg',
+        user: 'dct_pg01',
         host: 'dct_pg01',
-        database: 'pg01',
+        database: 'dct_pg01',
         password: '123qwe',
         port: 5432
       });
       await client.connect()
-      const pg_res: any = await client.query('SELECT * FROM test');
+      const pg_res: any = await client.query('SELECT * FROM event_log');
       let rows: Array<object> = pg_res.rows;
       await client.end();
       return { rows };
